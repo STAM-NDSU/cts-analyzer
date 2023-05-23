44e73aba20470b2590e6e88464d4578bd389be22
Suraj Bhatta <suraj.b@arbyte.com.np>
2023-05-23 12:30:13 -0500

    fix refactor bug

--- analyzer/core.py
+++ analyzer/core.py
@@ -9,23 +9,28 @@ from . import config
 #  Analyze commit files to detect tests cases(functions and assertions) removed
 def analyze_test_cases_removal_in_commit_file(file: ModifiedFile, all_added_test_cases_in_commit: List) -> Collection[Tuple[str, bool]]:
     file_changes = file.diff
-    print(file.change_type)
+    # print(file.change_type)
     # print(file.old_path)
     # print(file.new_path)
     # print("_____")
-    for x in file.methods:
-        print(x.long_name)
-    print("_____")
-
-    # for x in file.methods_before:
-    #     print(x.name)
-
-    for x in file.methods_before:
-        print(x.name)
-
+    # methods = []
+    # for x in file.methods:
+    #     methods.append(x.name)
+        
     # print("_____")
+    # methods_before = []
+    # removed_methods = []
+    # for x in file.methods_before:
+    #     methods_before.append(x.name)
+    #     if x.name not in methods:
+    #         removed_methods.append(x.name)
+    # print("_____")
+    # print(removed_methods)
+    # changed_methods = []
     # for x in file.changed_methods:
-    #     print(x.name)
+    #     changed_methods.append(x.name)
+    # print("_____")
+    
     candidate_removed_test_functions = get_removed_test_functions(file_changes, file)
     candidate_added_test_functions = get_added_test_functions(file_changes, file)
     refactored_test_functions = get_refactored_test_functions(file_changes, file)
