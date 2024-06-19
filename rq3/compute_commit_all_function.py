07b83ca39e6d69a633ecee2b809290c19f38f5fe
Suraj Bhatta <bhattasuraj76@gmail.com>
2024-06-18 21:35:41 -0500

    test compute commit all functions utility function

--- rq3/compute_commit_all_function.py
+++ rq3/compute_commit_all_function.py
@@ -5,7 +5,7 @@ import sys
 
 sys.path.append("../")
 # Redirect console ouput to a file
-sys.stdout = open("../temp_rq3.txt", "w")
+# sys.stdout = open("../temp_rq3.txt", "w")
 
 import os
 from typing import Optional, List
@@ -24,15 +24,15 @@ from pathlib import Path
 import pandas as pd
 import lizard
 
-PROJECT_DIR = "./../os-java-projects/"
+PROJECT_DIR = "../../os-java-projects/"
 projects_list = [
-    "commons-lang",
+    # "commons-lang",
     "gson",
-    "commons-math",
-    "jfreechart",
-    "joda-time",
-    "pmd",
-    "cts",
+    # "commons-math",
+    # "jfreechart",
+    # "joda-time",
+    # "pmd",
+    # "cts",
 ]
 VALIDATION_DIR = "../io/validationFiles"
 current_state = os.getcwd()
@@ -101,16 +101,14 @@ for project in projects_list:
 
         all_commits_data[commit] = all_functions_in_commits
         print(all_commits_data)
-        f = open(
-            "../../rq3/all_commits_all_functions/"
-            + project
-            + "/"
-            + project
-            + "-"
-            + commit
-            + ".json",
-            "w",
-        )
+        
+        dirpath = "../../cts-analyzer/io/rq3/all_commits_all_functions/"+ project
+        filename = project + "-"  + commit+ ".json"
+            
+        if(os.path.exists(dirpath)== False):
+            os.mkdir(dirpath)
+        
+        f = open(dirpath + "/" + filename, "w")
         f.write(json.dumps(all_commits_data, indent=4))
     print("=========================")
 
